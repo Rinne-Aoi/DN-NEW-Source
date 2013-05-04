fa72c70a8aefaf5f898e7264f37193b887408b02
Dong Gil, Seo <sirospace@sirospace.com>
2013-05-04 23:37:45 +0900

    Fix Some Strings Error.

--- src/angeloid/dreamnarae/Donate.java
+++ src/angeloid/dreamnarae/Donate.java
@@ -1,13 +1,19 @@
 package angeloid.dreamnarae;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Donate extends Activity {
 
@@ -35,6 +41,9 @@ public class Donate extends Activity {
 	Button setting;
 	Button developerinfo;
 	Button donate;
+	
+	// Kakao Link / Story Link
+			private String encoding = "UTF-8";
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -86,16 +95,70 @@ public class Donate extends Activity {
 		developerinfo.setTypeface(MainActivity.Font);
 		donate.setTypeface(MainActivity.Font);
 
-		//gopaypal.setOnClickListener(new View.OnClickListener() {
+		gopaypal.setOnClickListener(new View.OnClickListener() {
 
-			//@Override
-			//public void onClick(View v) {
-			//	Uri uri = Uri
-			//			.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=GQE4HTNUAP6CS&lc=KR&item_name=Angeloid%20Team&item_number=DR&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted");
-			//	Intent it = new Intent(Intent.ACTION_VIEW, uri);
-			//	startActivity(it);
-		//	}
-		//});
+			@Override
+			public void onClick(View v) {
+				Uri uri = Uri
+						.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=GQE4HTNUAP6CS&lc=KR&item_name=Angeloid%20Team&item_number=DR&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted");
+				Intent it = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(it);
+			}
+		});
+	}
+	/**
+	 * Send App data
+	 */
+	public void sendAppData(View v) throws NameNotFoundException {
+		ArrayList<Map<String, String>> metaInfoArray = new ArrayList<Map<String, String>>();
+
+		// If application is support Android platform.
+		Map<String, String> metaInfoAndroid = new Hashtable<String, String>(1);
+		metaInfoAndroid.put("os", "android");
+		metaInfoAndroid.put("devicetype", "phone");
+		metaInfoAndroid.put("installurl",
+				"market://details?id=angeloid.dreamnarae");
+		metaInfoAndroid.put("executeurl", "kakaoLinkTest://starActivity");
+
+		// add to array
+		metaInfoArray.add(metaInfoAndroid);
+
+		// Recommended: Use application context for parameter.
+		KakaoLink kakaoLink = KakaoLink.getLink(getApplicationContext());
+
+		// check, intent is available.
+		if (!kakaoLink.isAvailableIntent()) {
+			alert("Not installed KakaoTalk.");
+			return;
+		}
+
+		/**
+		 * @param activity
+		 * @param url
+		 * @param message
+		 * @param appId
+		 * @param appVer
+		 * @param appName
+		 * @param encoding
+		 * @param metaInfoArray
+		 */
+		String app_name = getString(R.string.app_name);
+		String message = getString(R.string.kakaotalkmessage);
+		kakaoLink
+				.openKakaoAppLink(
+						this,
+						"market://details?id=angeloid.dreamnarae",
+						message,
+						getPackageName(),
+						getPackageManager().getPackageInfo(getPackageName(), 0).versionName,
+						app_name, encoding, metaInfoArray);
+	}
+
+	private void alert(String message) {
+		new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setTitle(R.string.app_name).setMessage(message)
+				.setPositiveButton(android.R.string.ok, null).create().show();
 	}
 
 	public void mainscreen(View v) {
@@ -149,7 +212,11 @@ public class Donate extends Activity {
 	}
 
 	public void promotingscreen(View v) {
-		Toast.makeText(this, R.string.promotingnot, Toast.LENGTH_SHORT).show();
+		try {
+			sendAppData(v);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void settingscreen(View v) {
